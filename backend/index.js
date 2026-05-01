0d46b97c6a47d392bded10d06d251d088ffde674
kamineni51 <sravanikamineni51@gmail.com>
2026-05-01 20:41:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -67,7 +67,6 @@ app.get('', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
-    console.log(`API Documentation available at http://localhost:${PORT}/api-docs`);
 });
